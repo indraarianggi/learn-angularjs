b0f9610d2d7916904e43fb5775fe1139da0d7a23
indraarianggi <indraarianggi@gmail.com>
2018-08-23 07:17:55 +0700

    angular directive - ng-repeat, ng-include, ng-show, filter

--- app/app.js
+++ app/app.js
@@ -12,6 +12,31 @@ ninjaApp.controller('NinjaController', ['$scope', function($scope) {
     
     $scope.message = "Helloo World!";
 
-    $scope.ninjas = ['Indra', 'Arianggi', 'Suryaatmaja', 'Candra']
+    $scope.ninjas = [
+        {
+            name: 'Indra',
+            belt: 'Black',
+            rate: 100,
+            available: true
+        },
+        {
+            name: 'Arianggi',
+            belt: 'Green',
+            rate: 30,
+            available: false
+        },
+        {
+            name: 'Suryaatmaja',
+            belt: 'Orange',
+            rate: 10,
+            available: true
+        },
+        {
+            name: 'Candra',
+            belt: 'Yellow',
+            rate: 50,
+            available: false
+        }
+    ];
 
 }]);
